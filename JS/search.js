b040481c6b752d8fcc519c3bc67fe35f977c0434
jorget2323 <jorget23@gmail.com>
2022-11-24 02:22:44 -0500

    Cambiando codigo para leer array

--- JS/search.js
+++ JS/search.js
@@ -1,18 +1,69 @@
-document.addEventListener("keyup", e=>{
+const restaurantes = [
+  {  
+      Nombre: 'La magnolia' , 
+      Direccion: 'Cr 34 # 28 - 25', 
+      Telefono: '3011234567'
+  },
+  {
+      Nombre: 'La Magdalena' , 
+      Direccion: ' Cr 43 # 82 - 52', 
+      Telefono: '3021234567'
+  },
+  {
+      Nombre: 'La Maria' , 
+      Direccion: 'Cr 45 # 39 - 25', 
+      Telefono: '3031234567'
+  },
+  {
+      Nombre: 'La Constansa' , 
+      Direccion: 'Cr 54 # 93 - 52', 
+      Telefono: '3041234567'
+  },
+  
+];
 
-    if (e.target.matches("#busqueda")){
-  
-        if (e.key ==="Escape")e.target.value = ""
-  
-        document.querySelectorAll(".restau").forEach(resta =>{
-  
-            resta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-              ?restau.classList.remove("filtro")
-              :restau.classList.add("filtro")
-        })
-  
-    }
-  
-  
-  })
+let nom = restaurantes.map(function(res){
+  return '<h3>Restaurante '+res.Nombre+'</h3>'
+});
 
+let dir = restaurantes.map(function(res2){
+  return '<p>Dirección: '+res2.Direccion+'</p>'
+});
+
+let tel = restaurantes.map(function(res3){
+  return '<p>Telefono: '+res3.Telefono+'</p>'
+});
+
+btn.onclick = function() {
+  var busqueda_2 = document.getElementById("busqueda").value;
+  var busqueda = busqueda_2.toUpperCase();
+  if (busqueda == 'LA MAGNOLIA') {
+      panel_1.innerHTML = nom[0];
+      panel_2.innerHTML = dir[0];
+      panel_3.innerHTML = tel[0];
+      panel_4.innerHTML = "";
+  }else
+  if (busqueda == 'LA MAGDALENA') {
+    panel_1.innerHTML = nom[1];
+    panel_2.innerHTML = dir[1];
+    panel_3.innerHTML = tel[1];
+      panel_4.innerHTML = "";
+  }else
+  if (busqueda == 'LA MARIA') {
+    panel_1.innerHTML = nom[2];
+    panel_2.innerHTML = dir[2];
+    panel_3.innerHTML = tel[2];
+      panel_4.innerHTML = "";
+  }else
+  if (busqueda == 'LA CONSTANSA') {
+    panel_1.innerHTML = nom[3];
+    panel_2.innerHTML = dir[3];
+    panel_3.innerHTML = tel[3];
+      panel_4.innerHTML = "";
+  }else {
+      panel_1.innerHTML = "";
+      panel_2.innerHTML = "";
+      panel_3.innerHTML = "";
+      panel_4.innerHTML = "Ese restaurante no esta registrado"
+  }
+}
